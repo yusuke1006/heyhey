e170e84d24caad3e316ac2fb7d535c49b7f43c67
yusuke1006 <lv6y06@icloud.com>
2020-11-28 10:09:38 +0900

    uetus

--- src/hoihoi/hello.java
+++ src/hoihoi/hello.java
@@ -1,10 +1,15 @@
 package hoihoi;
 
-public class heloo {
+public class hello {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("ghbvahbhkbasv");
+
+		for (int i = 0; i < args.length; i++) {
+			String string = args[i];
+
+		}
 	}
 
 }
